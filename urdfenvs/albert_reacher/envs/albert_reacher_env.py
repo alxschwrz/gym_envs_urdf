beba873f73f5707b823072068af0dd3c4c6e6782
Alexander <alexanderschwarz.ulm@gmail.com>
2022-06-21 11:32:42 +0200

    conversion of observation space to goalEnv (see openai goalenv) in order to use HER. get_ee_position function implemented

--- urdfenvs/albert_reacher/envs/albert_reacher_env.py
+++ urdfenvs/albert_reacher/envs/albert_reacher_env.py
@@ -2,14 +2,20 @@ import numpy as np
 
 from urdfenvs.albert_reacher.resources.albert_robot import AlbertRobot
 from urdfenvs.urdfCommon.urdf_env import UrdfEnv
+from forwardkinematics.urdfFks.albertFk import AlbertFk
 
 
 class AlbertReacherEnv(UrdfEnv):
     """Albert reacher environment."""
 
     def __init__(self, **kwargs):
-        super().__init__(AlbertRobot(), **kwargs)
+        super().__init__(robot=AlbertRobot(), task_list=["albert"], **kwargs)
+        self._goalEnv = True
+        self._fk = AlbertFk()
         self.set_spaces()
+        if self._goalEnv:
+            ob = self.reset()
+            self.convert_observation_space_to_goalEnv(ob['observation'].shape, tuple((self._goals[0].m(),)))
 
     def check_initial_state(self, pos, vel):
         if not isinstance(pos, np.ndarray) or not pos.size == self._robot.n()+1:
@@ -20,3 +26,19 @@ class AlbertReacherEnv(UrdfEnv):
         if not isinstance(vel, np.ndarray) or not vel.size == self._robot.n():
             vel = np.zeros(self._robot.n())
         return pos, vel
+
+    def get_ee_position(self):
+        joint_states = self._robot.get_observation()['joint_state']['position']
+        ee_position = self._fk.fk(joint_states, "panda_link9", positionOnly=True)
+        return ee_position
+
+    def convert_observation_space_to_goalEnv(self, observation_shape: tuple, goal_shape: tuple):
+        # todo: limits are hardcoded
+        import gym
+        self.observation_space = gym.spaces.Dict(
+            dict(
+                observation=gym.spaces.Box(-10.0, 10.0, shape=observation_shape, dtype=np.float32),
+                desired_goal=gym.spaces.Box(-10.0, 10.0, shape=goal_shape, dtype=np.float32),
+                achieved_goal=gym.spaces.Box(-10.0, 10.0, shape=goal_shape, dtype=np.float32),
+            )
+        )
--- urdfenvs/panda_reacher/envs/panda_reacher_env.py
+++ urdfenvs/panda_reacher/envs/panda_reacher_env.py
@@ -1,6 +1,7 @@
 import numpy as np
 from urdfenvs.panda_reacher.resources.panda_robot import PandaRobot
 from urdfenvs.urdfCommon.urdf_env import UrdfEnv
+from forwardkinematics.urdfFks.pandaFk import PandaFk
 
 
 class PandaReacherEnv(UrdfEnv):
@@ -8,7 +9,13 @@ class PandaReacherEnv(UrdfEnv):
         super().__init__(
             PandaRobot(gripper=gripper, friction=friction), **kwargs
         )
+        self._fk = PandaFk()
         self.set_spaces()
+        self._goalEnv = True  # OpenAI GoalEnv with dict observations: observation, achieved_goal, desired_goal
+        if self._goalEnv:
+            ob = self.reset()
+            self.convert_observation_space_to_goalEnv(ob['observation'].shape, tuple((self._goals[0].m(),)))
+
 
     def check_initial_state(self, pos, vel):
         if not isinstance(pos, np.ndarray) or not pos.size == self._robot.n():
@@ -22,3 +29,19 @@ class PandaReacherEnv(UrdfEnv):
         if not isinstance(vel, np.ndarray) or not vel.size == self._robot.n():
             vel = np.zeros(self._robot.n())
         return pos, vel
+
+    def get_ee_position(self):
+        joint_states = self._robot.get_observation()['joint_state']['position']
+        ee_position = self._fk.fk(joint_states, 7, positionOnly=True)
+        return ee_position
+
+    def convert_observation_space_to_goalEnv(self, observation_shape: tuple, goal_shape: tuple):
+        # todo: limits are hardcoded
+        import gym
+        self.observation_space = gym.spaces.Dict(
+            dict(
+                observation=gym.spaces.Box(-10.0, 10.0, shape=observation_shape, dtype=np.float32),
+                desired_goal=gym.spaces.Box(-10.0, 10.0, shape=goal_shape, dtype=np.float32),
+                achieved_goal=gym.spaces.Box(-10.0, 10.0, shape=goal_shape, dtype=np.float32),
+            )
+        )
