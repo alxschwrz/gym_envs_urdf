d78945b5a9b3f476d68fa61a736c7a1889da170f
Alexander <alexanderschwarz.ulm@gmail.com>
2022-06-21 13:04:20 +0200

    albert change of example for debugging purposes

--- examples/albert.py
+++ examples/albert.py
@@ -7,16 +7,23 @@ import warnings
 def main():
     env = gym.make("albert-reacher-vel-v0", dt=0.01, render=True)
     action = np.zeros(9)
-    action[0] = 0.2
+    action[0] = 0.0
     action[1] = 0.0
     action[5] = -0.0
-    n_steps = 100000
+    action[7] = 0.1
+    n_episodes = 15
+    n_steps = 1_000
     ob = env.reset(
         pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
     )
     print(f"Initial observation : {ob}")
-    for _ in range(n_steps):
-        ob, _, _, _ = env.step(action)
+    for ep in range(n_episodes):
+        ob = env.reset()
+        for i in range(n_steps):
+            action = env.action_space.sample()
+            ob, rew, _, _ = env.step(action)
+            if i % 10 == 1:
+                print(i, action[0:3], ob['achieved_goal'], ob['desired_goal'], rew)
 
 
 if __name__ == "__main__":
